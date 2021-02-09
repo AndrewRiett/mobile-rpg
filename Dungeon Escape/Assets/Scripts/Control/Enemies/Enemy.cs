9e66848185bd56739c2ec07d473c3157caf36e56
Andrew <timoshevichandrew@gmail.com>
2021-02-09 18:07:26 +0300

    Enemy abstract class set up

---
+++ Dungeon Escape/Assets/Scripts/Control/Enemies/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Dungeon.Movement.Waypoints;
+
+namespace Dungeon.Control.Enemies
+{
+    public abstract class Enemy : MonoBehaviour
+    {
+        [SerializeField] protected float maxHealth;
+        [SerializeField] protected float speed;
+        [SerializeField] protected int gemsAmount;
+
+        [Header("Patrol path:")]
+        [SerializeField] protected PatrolPath patrolPath;
+        [SerializeField] protected float toleranceDistance = 0.1f;
+
+        public PatrolPath PatrolPath { get => patrolPath; }
+        public float ToleranceDistance { get => toleranceDistance; }
+        internal int CurrentWaypointID { get; set; }
+
+        protected virtual void Attack() { }
+        protected virtual bool CheckForAggro()
+        {
+            return true;
+        }
+    }
+}

261f67038df0a32e7168a881ec9bff533b850b62
Andrew <timoshevichandrew@gmail.com>
2021-02-09 18:08:12 +0300

    Moss Giant set up

---
+++ Dungeon Escape/Assets/Scripts/Control/Enemies/MossGiantContoller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dungeon.Control.Enemies
+{
+    public class MossGiantContoller : Enemy
+    {
+        protected override void Attack()
+        {
+            base.Attack();
+            throw new System.NotImplementedException();
+        }
+    }
+}

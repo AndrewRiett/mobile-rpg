6074f41e9edc6617c235b611fe1b499e67c0af7b
Andrew <timoshevichandrew@gmail.com>
2021-02-25 17:08:05 +0300

    Chase behaviour is added

---
+++ Dungeon Escape/Assets/Scripts/Enemies/EnemyStates/EnemyChase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using Dungeon.FSM;
+using Dungeon.Fighting;
+using Dungeon.Movement;
+using Dungeon.Navigation;
+
+namespace Dungeon.Enemies.EnemyStates
+{
+    public class EnemyChase : BaseState
+    {
+        private readonly EnemyAI _enemy;
+        private readonly NavController _navController;
+        private readonly float _moveSpeed;
+
+        public EnemyChase(EnemyAI enemy, NavController navController, 
+            float moveSpeed) : base(enemy.gameObject)
+        {
+            _enemy = enemy;
+
+            _navController = navController;
+            _moveSpeed = moveSpeed;
+        }
+
+        public override Enum Tick()
+        {
+            // if _enemy.inAttackRange -> Attack
+            
+            Vector2 destination = _enemy.GetLastTargetPos();
+
+            if (_navController.IsAtPosition(destination))
+            {
+                _navController.Stop();
+                return EnemyStateType.Suspicion; // returns and waits for the suspicion timer 
+            }
+            
+            _navController.MoveTo(destination, _moveSpeed);
+            return EnemyStateType.Chase;
+        }
+
+        public override Enum GetStateType()
+        {
+            return EnemyStateType.Chase;
+        }
+
+        public override void OnStateEnter()
+        {
+            // Debug.Log("Chase");
+        }
+    }
+}
---
+++ Dungeon Escape/Assets/Scripts/Enemies/EnemyStates/EnemySuspicion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Dungeon.FSM;
+using Dungeon.Navigation;
+using UnityEngine;
+
+namespace Dungeon.Enemies.EnemyStates
+{
+    public class EnemySuspicion : BaseState
+    {
+        private readonly EnemyAI _enemy;
+        private readonly NavController _navController;
+
+        public EnemySuspicion(EnemyAI enemy) : base(enemy.gameObject)
+        {
+            _enemy = enemy;
+            _navController = _enemy.NavController;
+        }
+
+        public override Enum Tick()
+        {
+            if (_navController.ShouldPatrolAfter(_enemy.SuspicionTime))
+            {
+                return EnemyStateType.Patrol;
+            }
+
+            return EnemyStateType.Suspicion;
+        }
+
+        public override Enum GetStateType()
+        {
+            return EnemyStateType.Suspicion;
+        }
+
+        public override void OnStateExit()
+        {
+            base.OnStateExit();
+            _enemy.isAggravated = false;
+        }
+    }
+}

cdf736f8c18f74de802da8411597a23bce88dd96
Andrew <timoshevichandrew@gmail.com>
2021-02-25 17:02:42 +0300

    AggroCollider class for aggravating enemies

---
+++ Dungeon Escape/Assets/Scripts/Fighting/AggroCollider.cs
@@ -0,0 +1,30 @@
+﻿using Dungeon.Enemies;
+using UnityEngine;
+
+namespace Dungeon.Fighting
+{
+    public class AggroCollider : MonoBehaviour
+    {
+        // private ITarget player;
+        // private ITarget characterToInteract;
+
+        private EnemyAI enemy;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Enemy")) return;
+
+            Debug.Log("set target");
+            enemy = other.GetComponent<EnemyAI>();
+            enemy.SetTarget(gameObject);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (!other.CompareTag("Enemy")) return;
+
+            enemy = other.GetComponent<EnemyAI>();
+            enemy.RemoveTarget();
+        }
+    }
+}
